2ec007b101f3368501478a17e92b2079aeb2f481
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2024-11-13 20:45:21 +0100

    Exercici 6 amb error 404

--- Activitat_8/main.py
+++ Activitat_8/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 from typing import Optional
 
@@ -19,7 +19,11 @@ def read_item():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int):
+def read_item(item_id: int, response: Response):
+    # Suposem que estem buscant un ítem específic, si no el trobem, retornem un error 404.
+    if item_id != 1:  # Asumim que només tenim un ítem amb id 1.
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": "Item no trobat"}
     return {"item_id": item_id}
 
 @app.post("/items/")
